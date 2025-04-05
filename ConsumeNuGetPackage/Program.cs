a740531489075d6aa86c0e7709aeec2ebadf96c9
Coutre <147004409+Coutre@users.noreply.github.com>
2025-04-05 15:54:57 -0400

    Add files via upload

---
+++ ConsumeNuGetPackage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string testString = "Hello, World!";
+        bool startsWithUpper = testString.StartsWithUpper();  
+        Console.WriteLine($"Here is the input: {testString}" + $"\nStarts with uppercase: {startsWithUpper}");
+
+        string testString2 = "hello, World!";
+        bool startsWithUpper2 = testString2.StartsWithUpper();
+        Console.WriteLine($"Here is the input: {testString2}" + $"\nStarts with uppercase: {startsWithUpper2}");
+    }
+}
